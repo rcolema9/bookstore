1b7a93caf742dc0f8f3606ce696e9b51b7607f7c
Richard Coleman <coleman_richard2@bah.com>
2014-04-08 19:31:38 -0400

    Class 10, Lab 3 Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,20 +16,54 @@ before_action :set_book, only: [ :show, :edit, :update, :destroy ]
   end
 
 def create
-  @book = Book.new(book_params)
-  @book.save
-  redirect_to @book
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
 end
+
+#Replaced code class 10 lab 2
+#def create
+#  @book = Book.new(book_params)
+#  @book.save
+#  redirect_to @book
+#end
+
+def create
+    @book = Book.new(book_params)
+    if @book.save
+      #replaced redirect in class 10 lab 2
+      #redirect_to @book
+      redirect_to @book, notice: "#{@book.title} was created!"
+    else
+      render :new
+    end
+end
+
 
 def edit
   #@book = Book.find(params[:id])
 end
 
+# Replaced code on class 10, lab2
+#def update
+#  #@book = Book.find(params[:id])
+#  @book.update(book_params)
+#  redirect_to @book
+#end
+
 def update
-  #@book = Book.find(params[:id])
-  @book.update(book_params)
-  redirect_to @book
+    if @book.update(book_params)
+      #Replaced redirect in class 10 lab 3
+      #redirect_to @book
+      redirect_to @book, notice: "#{@book.title} was created!"
+    else
+      render :new
+    end
 end
+
 
 def destroy
   #@book = Book.find(params[:id])
