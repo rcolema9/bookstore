adb4dd9d7faa9497c8dd4d88e205dd40fbbaac43
Richard Coleman <coleman_richard2@bah.com>
2014-04-08 20:53:43 -0400

    Class 10, Lab 5 - Applying Layouts

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Bookstore::Application.routes.draw do
   #delete "/books/:id" => "books#destroy"
   
   resources :books
-
+  root 'books#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
